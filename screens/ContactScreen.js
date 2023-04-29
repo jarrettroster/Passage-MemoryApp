38d4a12339a4654761d569d09f9a4bd5c9726909
Jarrett Roster <jarrettroster@protonmail.com>
2023-04-29 00:12:26 -0600

    Week 1 Assignment

---
+++ screens/ContactScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { ScrollView} from 'react-native';
+import { Card, Text } from 'react-native-elements';
+
+
+const ContactScreen = () => {
+  return (
+    <ScrollView>
+      <Card wrapperStyle={{margin: 20}}>
+        <Card.Title>Contact Information</Card.Title>
+        <Card.Divider />
+        <Text>1 Nucamp Way{'\n'}Seattle, WA 98001{'\n'}U.S.A.</Text>
+        <Text style={{marginTop: 10}}>Phone: 1-206-555-1234</Text>
+        <Text>Email: campsites@nucamp.co</Text>
+      </Card>
+    </ScrollView>
+  );
+};
+
+export default ContactScreen;
